f0f745936af5ddf41d8e9b1ca8a6892b6d61a1c7
Madhuri Palanivelu <palsmadhu18@gmail.com>
2019-10-19 17:05:14 -0700

    Implemented the interval parse logic

--- ExpressionTree/src/intervals/TreeNode.java
+++ ExpressionTree/src/intervals/TreeNode.java
@@ -1,5 +1,4 @@
-package expression;
-
+package intervals;
 /**
  * Representing a single node in the expression tree.
  */
@@ -7,6 +6,10 @@ public final class TreeNode {
   String value;
   TreeNode left, right;
 
+  /**
+   * The constructor for the TreeNode.
+   * @param item The string value of the current node.
+   */
   TreeNode(String item) {
     value = item;
     left = right = null;
